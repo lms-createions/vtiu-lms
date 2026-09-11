1c380eaeb4401fb35a26f726b6034e4946190618
lms-creations <lydiatds03@gmail.com>
2026-09-11 17:03:01 +0000

    Add whiteboard_uuid column to models

--- models.py
+++ models.py
@@ -3438,6 +3438,8 @@ class Meeting(db.Model):
 
     meeting_code = db.Column(db.String(80), unique=True, index=True, nullable=False)
 
+    whiteboard_uuid = db.Column(db.String(120), unique=True, nullable=True, index=True)
+
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
 
     scheduled_start = db.Column(db.DateTime, nullable=True)
@@ -3910,5 +3912,3 @@ class StudentPromotion(db.Model):
             'notes': self.notes
 
         }
-
-    
